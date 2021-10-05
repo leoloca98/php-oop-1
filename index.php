78b97e98afd0706f3f942e7d9fbf0dcf78246c04
leoloca98 <leonardolocatelli98@gmail.com>
2021-10-05 23:50:39 +0200

    Add Traccia

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- 
+    
+1- Definire una classe 'Movie':
+-> all'interno della classe dichiarare delle variabili d'istanza
+-> all'interno della classe definire un costruttore
+-> all'interno della classe definire almeno un metodo
+2- istanziare almeno due oggetti 'Movie' e stampare a schermo i valori delle proprietà, possibilmente senza l'uso di var_dump  
+
+
+-->
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>php-oop-1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
