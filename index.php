d7166c6f56ff7ae7e55f1fe7b660fd7c4a2d866f
leoloca98 <leonardolocatelli98@gmail.com>
2021-10-07 16:47:00 +0200

    Complete

--- index.php
+++ index.php
@@ -8,22 +8,43 @@
 
 
 -->
+<?php
 
+// Classe
+class Movie
+{
+    public $name;
+    public $director;
+    public $rating;
+    public $recessed;
 
+    public function __construct($nome, $regista, $valutazione, $incasso)
+    {
+        $this->name = $nome;
+        $this->director = $regista;
+        $this->rating = $valutazione;
+        $this->recessed = $incasso;
+    }
 
-<!DOCTYPE html>
-<html lang="en">
+    public function isRecord($incasso = 2840000000)
+    {
+        $is_recessed_record = $this->recessed - $incasso;
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/style.css">
-    <title>php-oop-1</title>
-</head>
+        if ($is_recessed_record >= 0) {
+            return "$this->name è il film con il più grande incasso della storia. Aggiornato al 21/09/2021" . "</br>" . "<hr>";
+        } else return "$this->name non è il film con il più grande incasso della storia. Aggiornato al 21/09/2021" . "</br>" . "<hr>";
+    }
+}
 
-<body>
+// Istanza 1
+$movie_1 = new Movie("Iron Man", "Jon Favreau", "7.8/10", 585000000);
 
-</body>
+// Istanza 2
+$movie_2 = new Movie("Intestellar", "Christopher Nolan", "8.6/10", 675000000);
 
-</html>
+// Istanza 3
+$movie_3 = new Movie("Avatar", "James Cameron", "7.8/10", 2840000000);
+
+echo $movie_1->isRecord();
+echo $movie_2->isRecord();
+echo $movie_3->isRecord();
